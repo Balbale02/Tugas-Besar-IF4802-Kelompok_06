102f78d0ba59687f49d59730c4ec806439aca784
Balbale02 <Iqbalmaulanasastra30@gmail.com>
2025-11-22 09:52:17 +0700

    Add Keyword.h with NodeKeyword structure
    Define the NodeKeyword structure and createKeywordNode function.

---
+++ Tugas-Besar-IF4802-Kelompok_06/Keyword.h
@@ -0,0 +1,19 @@
+#ifndef KEYWORD_H
+#define KEYWORD_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct NodeKeyword {
+    string namaKeyword;
+    string kategori;         // Contoh: "Topik Utama", "Metode", "Tools"
+
+    NodeKeyword *nextKeyword;
+};
+//pancingan
+    NodeKeyword *createKeywordNode(string nama, string kat) {
+    NodeKeyword *newNode = new NodeKeyword();
+}
+
+#endif
