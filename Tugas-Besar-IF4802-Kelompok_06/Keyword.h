483ce24b3cb656426cdf1cf0201005272d82925c
Iqbal Maulana S <Iqbalmaulanasastra30@gmail.com>
2025-11-24 20:24:35 +0700

    Menambahkan prosedur dan fungsi baru ke Keyword.h
    Updated the structure and functions for keyword management.

--- Tugas-Besar-IF4802-Kelompok_06/Keyword.h
+++ Tugas-Besar-IF4802-Kelompok_06/Keyword.h
@@ -4,14 +4,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-struct NodeKeyword {
-    string namaKeyword;
-    string kategori;         // Contoh: "Topik Utama", "Metode", "Tools"
-
-    NodeKeyword *nextKeyword;
+struct InfotypeKeyword {
+    string namaKeyword;  // Contoh: "Machine Learning"
+    string kategori;     // Contoh: "Topic", "Method", "Tool"
+    int relevansi;       // Skor 0-100 seberapa pas keywordnya
 };
-//pancingan
-    NodeKeyword *createKeywordNode(string nama, string kat);
+
+typedef struct elementKeyword *addressKeyword;
+
+struct elementKeyword {
+    InfotypeKeyword info;
+    addressKeyword next;
+};
+
+void createElementKeyword(InfotypeKeyword data, addressKeyword &P); //Akhtar
+void insertFirstKeyword(addressKeyword &first, addressKeyword P);   //Akhtar
+void insertLastKeyword(addressKeyword &first, addressKeyword P);    //Iqbal
+void insertAfterKeyword(addressKeyword &first, addressKeyword P, addressKeyword prec);  //Iqbal
+
+void deleteFirstKeyword(addressKeyword &first, addressKeyword &P);  //Akhtar
+void deleteLastKeyword(addressKeyword &first, addressKeyword &P);   //Akhtar
+void deleteAfterKeyword(addressKeyword prec, addressKeyword &P);    //Iqbal
+
+addressKeyword findElementKeyword(addressKeyword first, string namaCari);   //Iqbal
+void printKeywords(addressKeyword first);   //Iqbal
 
 #endif
