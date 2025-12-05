e38f48375332a1d16bb69f336eb034921e9064be
Iqbal Maulana S <Iqbalmaulanasastra30@gmail.com>
2025-12-05 10:44:19 +0700

    Add files via upload

---
+++ Tugas-Besar-IF4802-Kelompok_06/Tugas-Besar-IF4802-Kelompok_06/Paper_103012400404.cpp
@@ -0,0 +1,23 @@
+#include "Paper.h"
+
+void createListPaper(ListPaper &L){
+    L.first = nullptr;
+    L.last  = nullptr;
+}
+
+void createElementPaper(InfotypePaper data, addressPaper &P){
+}
+
+void insertLastPaper(ListPaper &L, addressPaper P){
+}
+
+void deleteLastPaper(ListPaper &L, addressPaper &P){
+}
+
+addressPaper findElementPaper(ListPaper L, string judulCari){
+}
+void addKeywordToPaper(ListPaper &L, string judulPaper, InfotypeKeyword dataKey){
+}
+
+void deleteKeywordFromPaper(ListPaper &L, string judulPaper, string namaKey){
+}
