7d2c36bd5e9e5f1a51834a2ba2c95ca51929baa3
Iqbal Maulana S <Iqbalmaulanasastra30@gmail.com>
2025-12-05 11:20:04 +0700

    Implement paper list management functions

--- Tugas-Besar-IF4802-Kelompok_06/Paper_103012400404.cpp
+++ Tugas-Besar-IF4802-Kelompok_06/Paper_103012400404.cpp
@@ -1,23 +1,76 @@
 #include "Paper.h"
 
-void createListPaper(ListPaper &L){
+void createListPaper(ListPaper &L) {
     L.first = nullptr;
-    L.last  = nullptr;
+    L.last = nullptr;
 }
 
-void createElementPaper(InfotypePaper data, addressPaper &P){
+void createElementPaper(InfotypePaper data, addressPaper &P) {
+    P = new elementPaper;
+    P->info = data;
+
+    P->next = nullptr;
+    P->prev = nullptr;
+
+    P->firstKeyword = nullptr;
 }
 
-void insertLastPaper(ListPaper &L, addressPaper P){
+void insertLastPaper(ListPaper &L, addressPaper P) {
+    if (L.first == nullptr) {
+        L.first = P;
+        L.last = P;
+    } else {
+        P->prev = L.last;
+        L.last->next = P;
+        L.last = P;
+    }
 }
 
-void deleteLastPaper(ListPaper &L, addressPaper &P){
+void deleteLastPaper(ListPaper &L, addressPaper &P) {
+    P = L.last;
+    if (L.first == nullptr) {
+        P = nullptr;
+        cout <<"List masih kosong";
+    }else if (L.first == L.last) {
+        L.first = nullptr;
+        L.last = nullptr;
+    } else {
+        L.last = L.last->prev;
+        L.last->next = nullptr;
+        P->prev = nullptr;
+    }
 }
 
-addressPaper findElementPaper(ListPaper L, string judulCari){
-}
-void addKeywordToPaper(ListPaper &L, string judulPaper, InfotypeKeyword dataKey){
+addressPaper findElementPaper(ListPaper L, string judulCari) {
+    addressPaper P = L.first;
+
+    while (P != nullptr) {
+        if (P->info.judul == judulCari) {
+            return P;
+        }
+        P = P->next;
+    }
+    return nullptr;
 }
 
-void deleteKeywordFromPaper(ListPaper &L, string judulPaper, string namaKey){
+void addKeywordToPaper(ListPaper &L, string judulPaper, InfotypeKeyword dataKey) {
+    addressPaper P = findElementPaper(L, judulPaper);
+
+    if (P == nullptr) {
+        cout << "[Error] Paper '" << judulPaper << "' tidak ditemukan." << endl;
+    } else {
+        // Nanti panggil fungsi child di sini
+        // insertLastKeyword(P->firstKeyword, ...);
+        cout << "[Info] Paper ditemukan, siap insert keyword." << endl;
+    }
+}
+
+void deleteKeywordFromPaper(ListPaper &L, string judulPaper, string namaKey) {
+    addressPaper P = findElementPaper(L, judulPaper);
+
+    if (P == nullptr) {
+         cout << "[Error] Paper '" << judulPaper << "' tidak ditemukan." << endl;
+    } else {
+        cout << "[Info] Paper ditemukan, siap delete keyword." << endl;
+    }
 }
