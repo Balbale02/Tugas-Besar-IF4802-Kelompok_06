ce8eacc7b52d9c10bf2448489483f333a9c5f517
akhtarsdq <akhtarsdq2018@gmail.com>
2025-12-06 14:45:20 +0700

    Update Keyword_103012400089.cpp

--- Tugas-Besar-IF4802-Kelompok_06/Keyword_103012400089.cpp
+++ Tugas-Besar-IF4802-Kelompok_06/Keyword_103012400089.cpp
@@ -1,1 +1,53 @@
+#include "Paper.h"
+
+void createElementKeyword(InfotypeKeyword data, addressKeyword &p){
+    p = new elementKeyword;
+    p->info.namaKeyword = data.namaKeyword;
+    p->info.kategori = data.kategori;
+    p->info.relevansi = data.relevansi;
+    p->next = nullptr;
+}
+
+void insertFirstKeyword(addressKeyword &first, addressKeyword P){
+    if(first == nullptr){
+        first = P;
+    }else{
+        P->next = first;
+        first = P;
+    }
+}
+
+void deleteFirstKeyword(addressKeyword &first, addressKeyword &P){
+    if(first == nullptr){
+        cout << "List keyword masih kosong" << endl;
+        P = nullptr;
+    }else if(first->next == nullptr){
+        P = first;
+        first = nullptr;
+    }else{
+        P = first;
+        first = P->next;
+        P->next = nullptr;
+    }
+}
+
+void deleteLastKeyword(addressKeyword &first, addressKeyword &P){
+    if(first == nullptr){
+        cout << "List keyword masih kosong" << endl;
+        P = nullptr;
+    }else if(first->next == nullptr){
+        P = first;
+        first = nullptr;
+    }else{
+        addressKeyword q;
+
+        q = first;
+        while(q->next->next != nullptr){
+            q = q->next;
+        }
+        P = q->next;
+        q->next = nullptr;
+    }
+}
+
 
