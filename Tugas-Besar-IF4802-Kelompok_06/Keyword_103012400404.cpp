522cda665bd176feb2eaedd1a2ee4de2da179f32
Iqbal Maulana S <Iqbalmaulanasastra30@gmail.com>
2025-12-09 22:37:42 +0700

    Implement keyword management functions

--- Tugas-Besar-IF4802-Kelompok_06/Keyword_103012400404.cpp
+++ Tugas-Besar-IF4802-Kelompok_06/Keyword_103012400404.cpp
@@ -1,1 +1,73 @@
 #include "paper.h"
+
+void insertLastKeyword(addressKeyword &first, addressKeyword P) {
+    if (first == nullptr) {
+        first = P;
+    } else {
+        addressKeyword last = first;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = P;
+    }
+}
+
+void insertAfterKeyword(addressKeyword &first, addressKeyword P, addressKeyword prec) {
+    if (prec != nullptr) {
+        P->next = prec->next;
+        prec->next = P;
+    }
+}
+
+addressKeyword findElementKeyword(addressKeyword first, string namaCari) {
+    addressKeyword P = first;
+    while (P != nullptr) {
+        if (P->info.namaKeyword == namaCari) {
+            return P;
+        }
+        P = P->next;
+    }
+
+    return nullptr;
+}
+
+void printKeywords(addressKeyword first) {
+    if (first == nullptr) {
+        cout << "\t(Tidak ada keyword)" << endl;
+    } else {
+        addressKeyword P = first;
+        int nomor = 1;
+
+        while (P != nullptr) {
+            cout << "\tKEYWORD KE-" << nomor << endl;
+            cout << "\tNama keyword: " << P->info.namaKeyword << endl;
+            cout << "\tKategori    : " << P->info.kategori << endl;
+            cout << "\tRelevansi   : " << P->info.relevansi << "/10" << endl;
+            cout << endl;
+
+            P = P->next;
+            nomor++;
+        }
+    }
+}
+
+void sortKeywordsInPaper(addressPaper P) {
+    if (P == nullptr || P->firstKeyword == nullptr) return;
+
+    bool swapped = true;
+    while (swapped) {
+        swapped = false;
+        addressKeyword K = P->firstKeyword;
+
+        while (K->next != nullptr) {
+            if (K->info.namaKeyword > K->next->info.namaKeyword) {
+                InfotypeKeyword temp = K->info;
+                K->info = K->next->info;
+                K->next->info = temp;
+
+                swapped = true;
+            }
+            K = K->next;
+        }
+    }
+}
