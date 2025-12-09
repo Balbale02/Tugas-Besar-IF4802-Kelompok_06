8c6fb2d1e19610d0c88b1fab9f89efcbefbdad99
Iqbal Maulana S <Iqbalmaulanasastra30@gmail.com>
2025-12-09 22:51:57 +0700

    Refactor Paper management functions

--- Tugas-Besar-IF4802-Kelompok_06/Paper_103012400404.cpp
+++ Tugas-Besar-IF4802-Kelompok_06/Paper_103012400404.cpp
@@ -55,14 +55,55 @@ addressPaper findElementPaper(ListPaper L, string judulCari) {
 
 void addKeywordToPaper(ListPaper &L, string judulPaper, InfotypeKeyword dataKey) {
     addressPaper P = findElementPaper(L, judulPaper);
-
     if (P == nullptr) {
         cout << "[Error] Paper '" << judulPaper << "' tidak ditemukan." << endl;
-    } else {
-        // Nanti panggil fungsi child di sini
-        // insertLastKeyword(P->firstKeyword, ...);
-        cout << "[Info] Paper ditemukan, siap insert keyword." << endl;
+        return;
     }
+    addressKeyword K_Baru;
+    createElementKeyword(dataKey, K_Baru);
+    int pilihan = 0;
+    cout << "\n>>> OPSI TAMBAH KEYWORD KE: " << P->info.judul << " <<<" << endl;
+    cout << "1. Insert First (Paling Depan)" << endl;
+    cout << "2. Insert Last (Paling Belakang)" << endl;
+    cout << "3. Insert After (Setelah Keyword Tertentu)" << endl;
+    cout << "Pilihan Anda (1-3): ";
+    cin >> pilihan;
+
+    if (pilihan == 1) {
+        // --- Insert First ---
+        insertFirstKeyword(P->firstKeyword, K_Baru);
+        cout << "[Sukses] Keyword ditambahkan di awal." << endl;
+    } else if (pilihan == 2) {
+        // --- Insert Last ---
+        insertLastKeyword(P->firstKeyword, K_Baru);
+        cout << "[Sukses] Keyword ditambahkan di akhir." << endl;
+    } else if (pilihan == 3) {
+        // --- Insert After ---
+        if (P->firstKeyword == nullptr) {
+            cout << "[Info] List kosong, otomatis insert first." << endl;
+            insertFirstKeyword(P->firstKeyword, K_Baru);
+        } else {
+            string namaPrec;
+            cout << "Masukkan nama keyword sebelumnya (Predecessor): ";
+            cin >> ws; // Handle buffer enter
+            getline(cin, namaPrec);
+            addressKeyword Prec = findElementKeyword(P->firstKeyword, namaPrec);
+
+            if (Prec != nullptr) {
+                insertAfterKeyword(P->firstKeyword, K_Baru, Prec);
+                cout << "[Sukses] Keyword ditambahkan setelah '" << namaPrec << "'." << endl;
+            } else {
+                cout << "[Gagal] Keyword '" << namaPrec << "' tidak ditemukan. Batal insert." << endl;
+                return;
+            }
+        }
+    } else {
+        cout << "[Error] Pilihan tidak valid." << endl;
+        return;
+    }
+    cout << "\n[UPDATE DATA PAPER: " << P->info.judul << "]" << endl;
+    printKeywords(P->firstKeyword);
+    cout << "---------------------------------------------" << endl;
 }
 
 void deleteKeywordFromPaper(ListPaper &L, string judulPaper, string namaKey) {
@@ -70,7 +111,80 @@ void deleteKeywordFromPaper(ListPaper &L, string judulPaper, string namaKey) {
 
     if (P == nullptr) {
          cout << "[Error] Paper '" << judulPaper << "' tidak ditemukan." << endl;
-    } else {
-        cout << "[Info] Paper ditemukan, siap delete keyword." << endl;
+         return;
     }
+    addressKeyword firstK = P->firstKeyword;
+    addressKeyword K_Hapus = nullptr;
+
+    if (firstK == nullptr) {
+        cout << "[Info] Tidak ada keyword untuk dihapus." << endl;
+        return;
+    }
+    if (firstK->info.namaKeyword == namaKey) {
+        deleteFirstKeyword(P->firstKeyword, K_Hapus);
+        cout << "[Sukses] Keyword '" << namaKey << "' dihapus dari depan." << endl;
+    }
+    else {
+        addressKeyword Prec = firstK;
+        bool ketemu = false;
+        while (Prec->next != nullptr) {
+            if (Prec->next->info.namaKeyword == namaKey) {
+                ketemu = true;
+                break;
+            }
+            Prec = Prec->next;
+        }
+        if (ketemu) {
+            deleteAfterKeyword(Prec, K_Hapus);
+            cout << "[Sukses] Keyword '" << namaKey << "' berhasil dihapus." << endl;
+
+        } else {
+            cout << "[Gagal] Keyword '" << namaKey << "' tidak ditemukan." << endl;
+            return;
+        }
+    }
+
+    if (K_Hapus != nullptr) {
+        delete K_Hapus;
+    }
+
+    cout << "\n[UPDATE DATA PAPER: " << P->info.judul << "]" << endl;
+    printKeywords(P->firstKeyword);
+    cout << "---------------------------------------------" << endl;
+}
+
+void sortPapersByTitle(ListPaper &L) {
+    if (L.first == nullptr || L.first->next == nullptr) return
+
+    bool swapped = true;
+    while (swapped) {
+        swapped = false;
+        addressPaper P = L.first;
+
+        while (P->next != nullptr) {
+            if (P->info.judul > P->next->info.judul) {
+                InfotypePaper tempInfo = P->info;
+                P->info = P->next->info;
+                P->next->info = tempInfo;
+
+                addressKeyword tempKey = P->firstKeyword;
+                P->firstKeyword = P->next->firstKeyword;
+                P->next->firstKeyword = tempKey;
+
+                swapped = true;
+            }
+            P = P->next;
+        }
+    }
+}
+
+void sortEverythingAscending(ListPaper &L) {
+    sortPapersByTitle(L);
+    addressPaper P = L.first;
+    while (P != nullptr) {
+        sortKeywordsInPaper(P);
+
+        P = P->next;
+    }
+    cout << "[Success] Paper dan Keyword sudah diurutkan A-Z." << endl;
 }
