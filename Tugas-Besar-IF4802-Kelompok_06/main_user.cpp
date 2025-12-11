5c432c87b227e9f5e738292d1eda00721ea4b901
akhtarsdq <akhtarsdq2018@gmail.com>
2025-12-12 02:10:37 +0700

    Update main_user.cpp

--- Tugas-Besar-IF4802-Kelompok_06/main_user.cpp
+++ Tugas-Besar-IF4802-Kelompok_06/main_user.cpp
@@ -2,13 +2,19 @@
 #include "Paper.h"
 
 void menuUser(){
+    ListPaper L;
+    addressPaper P, q;
+    InfotypePaper dataPaper;
     int option=-99;
+    
+    createListPaper(L);
+    
     while (option != 0) {
         system("cls");
         cout << "===========================Menu=============================  " << endl;
-        cout << "|| 1. studi case 1                                         ||" << endl;
+        cout << "|| 1. Insert paper berdasarkan tahun terbit                ||" << endl;
         cout << "|| 2. studi case 2                                         ||" << endl;
-        cout << "|| 3. studi case 3                                         ||" << endl;
+        cout << "|| 3. Insert Keyword berdasarkan besar relevansi                                         ||" << endl;
         cout << "|| 4. studi case 4                                         ||" << endl;
         cout << "|| 5. studi case 5                                         ||" << endl;
         cout << "|| 6. studi case 6                                         ||" << endl;
@@ -21,8 +27,51 @@ void menuUser(){
         cin >> option;
         switch(option) {
            case 1  :
-               cout << "you choose option 1" << endl;
-               // kode
+              addressPaper q;
+              
+              cout << "Paper akan dimasukan dari paper yang paling lama diterbitkan hingga yang terbaru" << endl;
+              cout << "Silahkan masukan data elemen parent" << endl;
+              cout << "Judul: ";
+              cin >> dataPaper.judul;
+              cout << "Doi: ";
+              cin >> dataPaper.doi;
+              cout << "Penulis: ";
+              cin >> dataPaper.penulis;
+              cout << "Email: ";
+              cin >> dataPaper.email;
+              cout << "Afiliasi: ";
+              cin >> dataPaper.afiliasi;
+              cout << "Tahun terbit: ";
+              cin >> dataPaper.tahunTerbit;
+              
+              createElementPaper(dataPaper, P);
+              
+              q = L.first;
+              if(q == nullptr){
+                insertFirstPaper(L, P);
+                cout << "Paper berhasil dimasukan dalam list" << endl;
+                return;
+              }
+              
+              if(dataPaper.tahunTerbit < q->info.tahunTerbit){
+                insertFirstPaper(L, P);
+                cout << "Paper berhasil dimasukan dalam list" << endl;
+                return;
+              }
+              
+              
+              while(q->next != nullptr){
+                if(dataPaper.tahunTerbit < q->next->info.tahunTerbit){
+                    insertAfterPaper(L, q, P);
+                    cout << "Paper berhasil dimasukan dalam list" << endl;
+                    return;
+                }
+                q = q->next;
+              }
+              
+              insertLastPaper(L, P);
+              cout << "Paper berhasil dimasukan dalam list" << endl;
+
                break;
            case 2  :
                cout << "you choose option 2" << endl;
